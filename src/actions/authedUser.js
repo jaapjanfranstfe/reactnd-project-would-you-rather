d70adeab4a6e26ef16b2940ef32eefbb94c73d4c
Jaap-Jan Frans <jaapjan@gmail.com>
2018-10-25 20:39:53 +0200

    moved useransweredquestion action create to users actino file instead of in autheduser file

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,17 +1,8 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
-export const USER_ANSWER_QUESTION = 'USER_ANSWER_QUESTION'
 
 export function setAuthUser (id) {
     return {
         type: SET_AUTHED_USER,
         id
-    }
-}
-
-export function userAnswerQuestion (userId, qid) {
-    return {
-        type: USER_ANSWER_QUESTION,
-        userId,
-        qid
     }
 }
