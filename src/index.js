5a9c3c70eea8b1eb6ace2290bcbc4adaa9b3bad6
Jaap-Jan Frans <jaapjan@gmail.com>
2018-10-25 21:01:59 +0200

    fixed imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import combinedReducer from './reducers/index'
+import  middleware from './middleware'
+import App from './components/App'
+
+const store  = createStore(combinedReducer, middleware);
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>, document.getElementById('root')
 );
-
