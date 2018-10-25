4404a225f7e8435f7f449b1174708b85402fe27a
Jaap-Jan Frans <jaapjan@gmail.com>
2018-10-25 15:29:03 +0200

    added actions for the questions and the users an an shared action to load initial data with (after login of a user)

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getInitialData} from "../utils/api";
+import { receiveUsers } from "./users";
+import { receiveQuestions} from "./questions";
+import { showLoading, hideLoading } from 'react-redux-loading';
+
+export function handleInitialData () {
+    return (dispatch) => {
+        dispatch(showLoading());
+        return getInitialData()
+            .then(({ users, questions }) => {
+                dispatch(receiveQuestions(questions));
+                dispatch(receiveUsers(users));
+                dispatch(hideLoading());
+            })
+    }
+}
