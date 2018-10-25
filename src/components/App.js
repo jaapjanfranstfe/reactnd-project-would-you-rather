81df560c152bbd87e0d1fddfe3aa70a445d81a2d
Jaap-Jan Frans <jaapjan@gmail.com>
2018-10-25 21:38:24 +0200

    added login component (still empty )

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { connect } from 'react-redux'
+import { handleInitialData } from "../actions/shared";
+import Login from './Login'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Would you rather
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.props.dispatch(handleInitialData())
+    }
+
+    render() {
+        return (
+            <Router>
+              <Route path='/login' exact component={ Login }/>
+            </Router>
+        )
+    }
 }
 
-export default App;
+export default connect()(App)
---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withRouter} from 'react-router-dom'
+
+class Login extends Component {
+
+    render() {
+        return <div> LOGIN COMPONENT TODO</div>
+    }
+}
+
+export default withRouter(connect()(Login))
