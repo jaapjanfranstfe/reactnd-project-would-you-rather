063364c8964481b34b411c09431ea11a99d5eaaf
Jaap-Jan Frans <jaapjan@gmail.com>
2018-10-25 22:46:18 +0200

    get users from the store state and populate the select

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react'
+import React, {Component, Fragment} from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from "../actions/shared";
 import Login from './Login'
+import Grid from '@material-ui/core/Grid'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core/styles';
+import 'typeface-roboto';
 
 class App extends Component {
+
     componentDidMount() {
         this.props.dispatch(handleInitialData())
     }
 
     render() {
+        const theme = createMuiTheme({
+            typography: {
+                useNextVariants: true,
+            },
+        });
+
         return (
-            <Router>
-              <Route path='/login' exact component={ Login }/>
-            </Router>
+            <Fragment>
+                <CssBaseline/>
+                <Router>
+                    <MuiThemeProvider theme={theme}>
+                        <Grid container>
+                            <Grid item xs={12}>
+                                <Route path='/login' exact component={ Login }/>
+                            </Grid>
+                        </Grid>
+                    </MuiThemeProvider>
+                </Router>
+            </Fragment>
         )
     }
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,64 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter} from 'react-router-dom'
-
+import Card from '@material-ui/core/Card'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import CardContent from '@material-ui/core/CardContent'
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 class Login extends Component {
+    state = {
+        selectedUser: ''
+    };
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
 
     render() {
-        return <div> LOGIN COMPONENT TODO</div>
+        const { users } = this.props;
+
+        return <Grid container justify='center'>
+                <Grid item xs={6}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h3" gutterBottom>
+                                Welcome to 'Would you rather'!
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>
+                                Please select a user
+                            </Typography>
+
+                            <form autoComplete="off">
+                                <FormControl>
+                                    <InputLabel htmlFor="selectedUser">User</InputLabel>
+                                    <Select
+                                        value={this.state.selectedUser}
+                                        onChange={this.handleChange}
+                                        inputProps={{
+                                            name: 'selectedUser',
+                                            id: 'selectedUser'
+                                        }}
+                                    >
+                                        { users.map(user =>
+                                            <MenuItem key={user.id} value={user.id}>{user.name}</MenuItem>
+                                        )}
+                                    </Select>
+                                </FormControl>
+                            </form>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
     }
 }
 
-export default withRouter(connect()(Login))
+function mapStateToProps({ users }) {
+    return {
+        users: Object.values(users)
+    }
+}
+export default withRouter(connect(mapStateToProps)(Login))
